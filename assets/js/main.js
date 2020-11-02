37f19b5bb0eee593f82850cdfb3dd726295cf9e2
laurentvac <laurentvac80@gmail.com>
2020-11-02 09:15:58 +0100

    javapart2

---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+// // exo1 part2 les conditions-->
+// var number = prompt("number");
+
+// if ( number >= 21  ) {
+//     alert("bien ouej");
+// }
+// // exo2 part2-->
+// let age = prompt("ton âge");
+
+// if (age >= 18 ==true) {
+//     alert("vous êtes majeur");
+
+
+// }
+// else {
+//     alert("vous êtes mineur")
+// }
+// // exo3 -->
+// var number = prompt("ton nombre");
+
+// if (number%2 == 0 ){
+// alert("nombre paire");
+// }
+// else {
+//     alert("nombre impair");
+// }
+// exo 4-->
+// var note = prompt("donnez une note entre 0 et 10")
+// if (note <= 3) {
+//     alert("Nul");
+// }
+// // else if car entre 3 et 6
+// else if (note > 3 & note < 6) {
+//     alert("Moyen");
+// } else if (note >= 6 & note < 8) {
+//     alert("Assez bien");
+// } else if (note >= 8 & note == 9) {
+//     alert("Bien");
+// } else if (note >= 10) {
+//     alert("Excellent");
+// }
+// exo 5
+
+let day = 5;
+switch(day){
+    case 1:
+        alert("lundi");
+        break;
+    case 2:
+        alert("mardi");
+        break;
+    case 3:
+        alert("mercredi");
+        break;
+    case 4:
+        alert("jeudi");
+        break;
+    case 5:
+        alert("vendredi");
+        break;
+    case 6:
+        alert("samedi");
+        break;
+    case 7:
+        alert("dimanche");
+        break;
+        default:
+            alert("c'est pas bon")
+}
